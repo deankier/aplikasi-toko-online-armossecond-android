ea1db315d3e8469ac3bb96e8b4835d565f0cf88c
Galang Arsandy <galangarsandy@gmail.com>
2023-05-30 14:08:12 +0700

    Mengubah Splash Screen & Launcher

--- app/src/main/java/id/nerdcreative/armossecond/SplashActivity.java
+++ app/src/main/java/id/nerdcreative/armossecond/SplashActivity.java
@@ -6,17 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class SplashActifity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_actifity);
+        setContentView(R.layout.activity_splash_activity);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActifity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
         },1000);

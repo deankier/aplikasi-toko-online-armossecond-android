ed0b62c867dd30e209d37b9064681d882e334866
Galang Arsandy <galangarsandy@gmail.com>
2023-05-31 19:38:27 +0700

    Membuat Tampilan Login

--- app/src/main/java/id/nerdcreative/armossecond/LoginActivity.java
+++ app/src/main/java/id/nerdcreative/armossecond/LoginActivity.java
@@ -2,13 +2,22 @@ package id.nerdcreative.armossecond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView BtnForgot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        BtnForgot = findViewById(R.id.btn_forgot_login);
+
     }
 }
